cacaecb76007808fc8b8393c9ad6ea78ecbcad95
Luthuos <ecolau2017@gmail.com>
2022-05-13 12:25:44 -0300

    ejercicio 1

---
+++ level_1/src/ejercicio1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String user = scan.nextLine();
+
+        System.out.println("HELLO " + user + "!!!");
+    }
+}
