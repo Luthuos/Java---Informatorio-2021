0c6aa8da4bb4c32e2a90766a5bee83c2719e7ee5
Luthuos <ecolau2017@gmail.com>
2022-05-23 11:16:54 -0300

    ejercicio 7 terminado

--- level_1/src/ejercicio7.java
+++ level_1/src/ejercicio7.java
@@ -3,7 +3,19 @@ import java.util.Scanner;
 public class ejercicio7 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Ingrese palabra \n ->");
+        String palabra = scan.nextLine();
 
+        char palabraArray[] = palabra.toCharArray();
 
+        for (int i = 0; i<palabraArray.length; i++){
+            if (palabraArray[i] >= 'a' && palabraArray[i] <= 'z'){
+                palabraArray[i] = (char) ((int) palabraArray[i] - 32);
+            }
+        }
+
+        for (int i = 0; i < palabraArray.length; i++) {
+            System.out.print(palabraArray[i]);
+        }
     }
 }
